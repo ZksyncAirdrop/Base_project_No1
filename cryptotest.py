4d416904c98782059fea176b5e6cecfd45c0f7ce
Vortex <119066306+ZksyncAirdrop@users.noreply.github.com>
2026-06-26 00:59:41 +0330

    42th update

--- cryptotest.py
+++ cryptotest.py
@@ -72,3 +72,4 @@ wallet_checksum = w3.to_checksum_address(wallet_address)
 contract_checksum = w3.to_checksum_address(contract_address)
 encoded_function = contract.encode_abi("set", args=[123])
 current_block = w3.eth.block_number
+syncing_status = w3.eth.syncing
