30fe1f82a50bbaea5a0b097a4529122c8cd40892
Vortex <119066306+ZksyncAirdrop@users.noreply.github.com>
2026-06-26 00:27:34 +0330

    18th update

--- cryptotest.py
+++ cryptotest.py
@@ -48,3 +48,4 @@ current_gas_price = w3.eth.gas_price
 max_priority_fee = w3.eth.max_priority_fee
 pending_nonce = w3.eth.get_transaction_count(wallet_address, "pending")
 latest_block_data = w3.eth.get_block("latest")
+block_timestamp = latest_block_data["timestamp"]
