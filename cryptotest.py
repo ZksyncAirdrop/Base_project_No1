d0fad677a5196b00ad5fb466ed02691b236f77d3
Vortex <119066306+ZksyncAirdrop@users.noreply.github.com>
2026-06-26 00:18:36 +0330

    4th Update of first commit

--- cryptotest.py
+++ cryptotest.py
@@ -34,3 +34,4 @@ print("Transaction Hash:", tx_hash.hex())
 receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 print("Block Number:", receipt.blockNumber)
 chain_id = w3.eth.chain_id
+latest_block = w3.eth.block_number
