101b514d59616182409d9872026bea03ff98fc0e
Vortex <119066306+ZksyncAirdrop@users.noreply.github.com>
2026-06-26 00:10:32 +0330

    This is my First commit

---
+++ cryptotest.py
@@ -0,0 +1,33 @@
+```python
+from web3 import Web3
+
+w3 = Web3(Web3.HTTPProvider("https://sepolia.infura.io/v3/YOUR_INFURA_ID"))
+
+private_key = "YOUR_PRIVATE_KEY"
+wallet_address = "0xYourWalletAddress"
+contract_address = "0xYourContractAddress"
+
+abi = [
+    {
+        "inputs": [{"name": "_value", "type": "uint256"}],
+        "name": "set",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+
+contract = w3.eth.contract(address=contract_address, abi=abi)
+
+tx = contract.functions.set(123).build_transaction({
+    "from": wallet_address,
+    "nonce": w3.eth.get_transaction_count(wallet_address),
+    "gas": 100000,
+    "gasPrice": w3.eth.gas_price
+})
+
+signed_tx = w3.eth.account.sign_transaction(tx, private_key)
+tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+
+print("Transaction Hash:", tx_hash.hex())
+```
